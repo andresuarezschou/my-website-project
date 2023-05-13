beeba445d91498fb0341b19d239aadb8dc52c5f5
suarezschou <98514958+suarezschou@users.noreply.github.com>
2023-05-14 01:23:17 +0200

    removed unnecessary

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,13 @@ import {AiFillLinkedin, AiFillYoutube, AiFillFacebook, AiFillGithub} from "react
 import Image from "next/image";
 import logo from "../public/logo.svg"
 
+
 export default function Home() {
   return (
     <main className="bg-white px-10">
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between">
-          <h1 className="text-xl font-burtons">Andre's portfolio website</h1>
-          <ul className="flex items-center">
-            <li>
-              
-            </li>
-            <li>
-              <a className="bg-gradient-to-r from-cyan-500 to-teal-200 text-white px-4 rounded-md" 
-              href="../public/multimedia_curriculum.pdf" download target="_blank"
-              >
-                CV
-                </a>
-            </li>
-          </ul>
+          <h1 className="text-xl">Andre's portfolio website</h1>
         </nav>
         <div className="text-center p-10">
           <h2 className="text-5xl py-2 text-teal-600 font-medium">Andre Suarez Schou</h2>
@@ -28,10 +17,10 @@ export default function Home() {
             I code applications in react js and tailwind css.
           </p>
         </div>
-        <div className="text-5xl flex justify-center text-purple-800">
+        <div className="text-3xl flex justify-center text-purple-800 py-10">
           <a href="https://musical-twilight-75b4a5.netlify.app/">size guide application</a>
         </div>
-        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+        <div className="text-7xl flex justify-center gap-16 py-3 text-gray-600">
           <a href="https://www.facebook.com/suarezschou"><AiFillFacebook /></a>
           <a href="https://www.linkedin.com/in/andre-suarez-schou/"><AiFillLinkedin /></a>
           <a href="https://www.youtube.com/suarezschou"><AiFillYoutube /></a>
