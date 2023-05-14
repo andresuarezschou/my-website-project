1b20e0aabf77d04489a8884384215655773a8c6e
suarezschou <98514958+suarezschou@users.noreply.github.com>
2023-05-14 16:36:10 +0200

    margin bottom for logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {AiFillLinkedin, AiFillYoutube, AiFillFacebook, AiFillGithub} from "react-icons/ai"
+import {AiFillLinkedin, AiFillGithub} from "react-icons/ai"
 import Image from "next/image";
 import logo from "../public/logo.svg"
 
@@ -14,19 +14,20 @@ export default function Home() {
           <h2 className="text-5xl py-2 text-teal-600 font-medium">Andre Suarez Schou</h2>
           <h3 className="text-2xl py-2">web developer</h3>
           <p className="text-md py-5 leading-8 text-gray-700">
-            I code applications in react js and tailwind css.
+            I build web applications.
+            I was introduced to HTML, CSS, and the JavaScript library react by my school.
+            These days I like to use the Next.js framework and tailwind css.
+            I want to keep improving and develop some more complex applications.
           </p>
         </div>
         <div className="text-3xl flex justify-center text-purple-800 p-10">
           <a href="https://musical-twilight-75b4a5.netlify.app/">size guide application</a>
         </div>
         <div className="text-5xl flex justify-between py-5 text-blue-600">
-          <a href="https://www.facebook.com/suarezschou"><AiFillFacebook /></a>
           <a href="https://www.linkedin.com/in/andre-suarez-schou/"><AiFillLinkedin /></a>
-          <a href="https://www.youtube.com/suarezschou"><AiFillYoutube /></a>
           <a href="https://www.github.com/suarezschou"><AiFillGithub /></a>
         </div>
-        <div className="mx-auto flex justify-center ">
+        <div className="mx-auto flex justify-center mb-20">
           <Image src={logo} alt={"image of my logo"}/>
         </div>
       </section>
