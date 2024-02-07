3d7db9ae5627f3c0e16e0666a2437c228fee7a92
André Suárez Schou <98514958+suarezschou@users.noreply.github.com>
2024-02-07 16:25:00 +0100

    Update page.tsx
    edited some text on the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import {AiFillLinkedin, AiFillGithub} from "react-icons/ai"
 import Image from "next/image";
-import logo from "../public/logo.svg"
 import Lottie from "lottie-react"
 import animationData from "../public/64110-web-development.json"
 
@@ -17,11 +16,10 @@ export default function Home() {
           <h2 className="text-5xl py-2 text-teal-600 font-medium">Andre Suarez Schou</h2>
           <h3 className="text-2xl py-2">web developer</h3>
           <p className="text-lg py-10 leading-8 text-gray-700">
-            I build web applications.
-            I was introduced to HTML, CSS, and the JavaScript library React by my school. 
-            I use Next.js React framework and tailwind CSS.
-            I want to keep improving and develop applications in the future.
-            I am fascinated in how with the help of APIs we are able to build applications more efficiently.
+            
+            Multimediadesign International Bussiness Academy Kolding, Denmark. 
+            HTML, CSS, Javascript.
+            Today I like to use the Next.js React framework and Tailwind CSS to keep it simple.
             
           </p>
           <div className="flex justify-center">
@@ -33,12 +31,9 @@ export default function Home() {
           <a href="https://reliable-tulumba-787b60.netlify.app">food recipe application</a>
           <a href="https://glittering-faloodeh-0f2668.netlify.app/">storefront commercejs</a>
         </div>
-        <div className="text-5xl flex justify-between py-5 text-blue-600">
+        <div className="text-5xl flex-col flex justify-center py-5 text-blue-600">
           <a href="https://www.linkedin.com/in/andre-suarez-schou/"><AiFillLinkedin /></a>
           <a href="https://www.github.com/suarezschou"><AiFillGithub /></a>
-        </div>
-        <div className="mx-auto flex justify-center mb-20">
-          <Image src={logo} alt={"image of my logo"}/>
         </div>
       </section>
     </main>
