33275a0d223dcc2d496fec62eb40e9dac87f5080
André Suárez Schou <98514958+suarezschou@users.noreply.github.com>
2024-05-18 13:18:59 +0200

    Update page.tsx
    buttons instead of normal link

--- app/page.tsx
+++ app/page.tsx
@@ -26,10 +26,17 @@ export default function Home() {
               <Lottie animationData={animationData} />
           </div>
         </div>
-        <div className="text-3xl flex-col flex justify-center text-purple-800 p-10">
-          <a href="https://musical-twilight-75b4a5.netlify.app/"> size guide application internship </a>
+        <div className="text-3xl flex-col flex justify-center items-center text-purple-800 p-10">
+          
+          <button class="bg-sky-500 hover:bg-sky-700 ...">
+            <a href="https://musical-twilight-75b4a5.netlify.app/"> size guide application internship </a>
+          </button>
+          
+          
           <a href="https://reliable-tulumba-787b60.netlify.app"> food recipe application web dev IBA </a>
+          
           <a href="https://glittering-faloodeh-0f2668.netlify.app/"> storefront example using Commerce.js </a>
+          
         </div>
         <div className="text-5xl flex justify-center py-5 text-blue-600">
           <a href="https://www.linkedin.com/in/andre-suarez-schou/"><AiFillLinkedin /></a>
