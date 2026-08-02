36a0491ac7d8b9cc5d9e3ae1a0d10e18cafbc84e
André Suárez Schou <98514958+andresuarezschou@users.noreply.github.com>
2026-08-02 18:33:30 +0200

    Replace Inter font with Chango font in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/components/theme/theme-provider"
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Chango } from 'next/font/google'
 
 export const metadata = {
   title: 'Personal portfolio website',
